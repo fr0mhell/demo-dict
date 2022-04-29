2b6a2cf9b1d0fd8359f93482b654bb55b2bba700
grigory <t1m0feev.grigorij@gmail.com>
2022-04-29 23:53:51 +0300

    Demo dict v2
    Resolve hash collision with open addressing and linear probing

--- demo_dict.py
+++ demo_dict.py
@@ -1,10 +1,9 @@
 class DemoDict:
+    _initial_size = 8
 
     def __init__(self, **kwargs):
         # array that stores hash table indexes of insertion
-        self._locators_list = [None] * 8
-        # Count dictionary items
-        self._items_counter = 0
+        self._locators_list = [None] * self._initial_size
         # hash table
         self._table = []
 
@@ -22,32 +21,65 @@ class DemoDict:
         return 0 if not self._table else len(self._table)
 
     def _add_item(self, key, value):
-        if self._items_counter > self._locator_list_size / 2:
-            self._locators_list += [None] * self._locator_list_size
+        if len(self) > self._locator_list_size / 2:
+            self._double_locator_list()
 
-        # TODO: check if locator index is already busy and resolve collision
-        locator_index = hash(key) % self._locator_list_size
-        self._locators_list[locator_index] = self._next_table_index
+        locator_index, table_index = self._get_locator_index_and_table_row_index_by_key(key)
 
-        self._table.append([
-            hash(key),
-            key,
-            value,
-        ])
+        if table_index is None:
+            self._locators_list[locator_index] = self._next_table_index
+            self._table.append([hash(key), key, value])
+        else:
+            self._table[table_index] = [hash(key), key, value]
+
+    def _double_locator_list(self):
+        """Doubles locator list with empty cells."""
+        self._locators_list += [None] * self._locator_list_size
+
+    def _get_locator_index_and_table_row_index_by_key(self, key, locator_index=None):
+        """Get locator index and hash table row number by key.
+
+        Resolve hash collision using open addressing and linear probing with step equal to 1.
+        https://en.wikipedia.org/wiki/Hash_table#Open_addressing
+
+        """
+        if locator_index is None:
+            locator_index = hash(key) % self._locator_list_size
+
+        table_row_index = self._locators_list[locator_index]
+
+        if table_row_index is None:
+            return locator_index, table_row_index
+
+        old_hash, old_key, old_value = self._table[table_row_index]
+        if old_key == key:
+            return locator_index, table_row_index
+
+        # Perform linear probing - check next locator index
+        locator_index = (locator_index + 1) % self._locator_list_size
+        return self._get_locator_index_and_table_row_index_by_key(key, locator_index)
 
     def items(self):
-        for row in self._table:
-            yield row[1], row[2]
+        for _, key, value in self._table:
+            yield key, value
+
+    def keys(self):
+        return [key for _, key, value in self._table]
+
+    def values(self):
+        return [value for _, key, value in self._table]
 
     def __len__(self):
         return len(self._table)
 
     def __getitem__(self, item):
-        locator_index = hash(item) % self._locator_list_size
-        table_row_index = self._locators_list[locator_index]
-        table_row = self._table[table_row_index]
-        # TODO: check hash and resolve collision
-        return table_row[2]
+        locator_index, table_index = self._get_locator_index_and_table_row_index_by_key(item)
+
+        if table_index is None:
+            raise KeyError(f'Key "{item}" not found')
+
+        hash_value, key, value = self._table[table_index]
+        return value
 
     def __setitem__(self, key, value):
         self._add_item(key, value)
--- test_demo_dict.py
+++ test_demo_dict.py
@@ -2,30 +2,72 @@ from unittest import TestCase
 from demo_dict import DemoDict
 
 
+class MyHashable:
+    """Allows testing hash collisions by easy creation of values with same hash."""
+    def __init__(self, hash_value):
+        self.hash = hash_value
+
+    def __hash__(self):
+        return self.hash
+
+
 class DemoDictTestCase(TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        cls.a = 1
-        cls.b = 2
+        cls.a_value = 1
+        cls.b_value = 2
+        cls.c_value = 3
+
+    def setUp(self) -> None:
+        self.empty = DemoDict()
+        self.with_values = DemoDict(a=self.a_value, b=self.b_value)
 
     def test_create_with_values(self):
-        to_test = DemoDict(a=self.a, b=self.b)
-
-        self.assertEqual(len(to_test), 2)
-        self.assertEqual(to_test['a'], self.a)
-        self.assertEqual(to_test['b'], self.b)
+        self.assertEqual(len(self.with_values), 2)
+        self.assertEqual(self.with_values['a'], self.a_value)
+        self.assertEqual(self.with_values['b'], self.b_value)
 
     def test_create_empty(self):
-        to_test = DemoDict()
-        self.assertEqual(len(to_test), 0)
+        self.assertEqual(len(self.empty), 0)
 
-    def test_set_item(self):
-        to_test = DemoDict()
-        to_test['a'] = self.a
-        to_test['b'] = self.b
+    def test_set_item_empty_dict(self):
+        self.empty['a'] = self.a_value
+        self.empty['b'] = self.b_value
 
-        self.assertEqual(len(to_test), 2)
-        self.assertEqual(to_test['a'], self.a)
-        self.assertEqual(to_test['b'], self.b)
+        self.assertEqual(len(self.empty), 2)
+        self.assertEqual(self.empty['a'], self.a_value)
+        self.assertEqual(self.empty['b'], self.b_value)
+
+    def test_set_item_overwrites_value(self):
+        self.with_values['a'] = self.c_value
+        self.with_values['b'] = self.a_value
+
+        self.assertEqual(len(self.with_values), 2)
+        self.assertEqual(self.with_values['a'], self.c_value)
+        self.assertEqual(self.with_values['b'], self.a_value)
+
+    def test_get_item_raises_error_when_no_key(self):
+        with self.assertRaises(KeyError):
+            self.with_values['c']
+
+    def test_hash_collision(self):
+        # create two future keys with same hash
+        # locator of a will be in _locators_list[-1]
+        a = MyHashable(hash_value=DemoDict._initial_size-1)
+        # locator of b will be in _locators_list[0] because _locators_list[-1] already points to a
+        b = MyHashable(hash_value=DemoDict._initial_size-1)
+
+        self.empty[a] = self.a_value
+        self.empty[b] = self.b_value
+
+        a_locator = self.empty._locators_list[-1]
+        a_hash, a_key, a_value = self.empty._table[a_locator]
+        self.assertEqual(a_key, a)
+        self.assertEqual(a_value, self.a_value)
+
+        b_locator = self.empty._locators_list[0]
+        b_hash, b_key, b_value = self.empty._table[b_locator]
+        self.assertEqual(b_key, b)
+        self.assertEqual(b_value, self.b_value)
 
